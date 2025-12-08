a640bd0354843ec22c46c7b82b7d36261761d9d5
zahranur4 <zahranurazijah23@gmail.com>
2025-12-08 14:50:16 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GudangWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
